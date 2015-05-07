03895be93d0881ffb6a4c1e077659bc2d96c8456
Marius Sturm <marius@graylog.com>
2015-05-07 17:40:49 +0200

    add Close() and severity consts

--- gelf/writer.go
+++ gelf/writer.go
@@ -74,6 +74,18 @@ var (
 	magicGzip    = []byte{0x1f, 0x8b}
 )
 
+// Syslog severity levels
+const (
+  LOG_EMERG   = int32(0)
+  LOG_ALERT   = int32(1)
+  LOG_CRIT    = int32(2)
+  LOG_ERR     = int32(3)
+  LOG_WARNING = int32(4)
+  LOG_NOTICE  = int32(5)
+  LOG_INFO    = int32(6)
+  LOG_DEBUG   = int32(7)
+)
+
 // numChunks returns the number of GELF chunks necessary to transmit
 // the given compressed buffer.
 func numChunks(b []byte) int {
@@ -207,6 +219,11 @@ func (w *Writer) WriteMessage(m *Message) (err error) {
 	}
 
 	return nil
+}
+
+// Close connection and interrupt blocked Read or Write operations
+func (w *Writer) Close() (error) {
+  return w.conn.Close()
 }
 
 /*
